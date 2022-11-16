d1fa58910789e598792a2d37787b4b38c698e29d
Lev Borodin <levborodin@MacBook-Air-Lev.local>
2022-11-16 16:57:43 +0200

    [features-js] responsible design completed

--- js/script.js
+++ js/script.js
@@ -1,1 +1,9 @@
 "use strict";
+
+const burgerBtn = document.querySelector('.menu__icon');
+const burgerMenu = document.querySelector('.menu__body');
+burgerBtn.addEventListener('click', function () {
+    document.body.classList.toggle('_lock')
+    burgerBtn.classList.toggle('_active');
+    burgerMenu.classList.toggle('_active');
+})
