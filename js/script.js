af3e0542b70fab9792ad6df05f4c4bbcbef5f752
Lev Borodin <levborodin@MacBook-Air-Lev.local>
2022-11-18 18:36:28 +0200

    [features-bugs] p design fixed / html errors fixed / buttons bg reworked

--- js/script.js
+++ js/script.js
@@ -9,6 +9,15 @@ burgerBtn.addEventListener('click', function () {
     burgerMenu.classList.toggle('_active');
 })
 
+//background for a button when it selected by user
+const menuBtns = Array.from(document.querySelectorAll('.btnsBg'));
+for(const elem of menuBtns){
+    if(elem.checked){
+        const btnBg = elem.closest('li');
+        btnBg.classList.toggle('_selected');
+    }
+}
+
 //getting information that has to be changed while button clicked
 const pageTitle = document.querySelector('.content__title > h1')
 const pageImage = document.querySelector('.content__imgage > img')
@@ -26,7 +35,7 @@ const solarSystem = [
     },
     {
         name: 'Venus',
-        image: 'img/MERCURY.jpg',
+        image: 'img/venus.jpg',
         description: [
             "Venus is the second planet from the sun and is the hottest planet in the solar system. Its thick atmosphere is extremely toxic and composed of sulfuric acid clouds, the planet is an extreme example of the greenhouse effect.",
             "The average temperature on Venus' surface is 900 F (465 C). At 92 bar, the pressure at the surface would crush and kill you. And oddly, Venus spins slowly from east to west, the opposite direction of most of the other planets.",
@@ -36,7 +45,7 @@ const solarSystem = [
     },
     {
         name: 'Mercury',
-        image: 'img/venus.jpg',
+        image: 'img/MERCURY.jpg',
         description: [
             "Mercury is the closest planet to the sun and the smallest planet in the solar system — it is only a little larger than Earth's moon. Mercury zips around the sun in only 88 days and because it is so close to our star (about two-fifths the distance between Earth and the sun).",
             "Mercury experiences dramatic changes in its day and night temperatures. Mercury temperatures can reach a scorching 840 F (450 C) in the day, which is hot enough to melt lead. Meanwhile, on the night side, temperatures drop to minus 290 F (minus 180 C).",
@@ -94,5 +103,11 @@ function changePage(item){
         document.body.classList.toggle('_lock');
         burgerBtn.classList.toggle('_active');
         burgerMenu.classList.toggle('_active');
+    }
+    for(const elem of menuBtns){
+        if(elem.checked){
+            const btnBg = elem.closest('li');
+            btnBg.classList.toggle('_selected');
+        }
     }
 }
